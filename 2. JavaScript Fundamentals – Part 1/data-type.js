fb76af2e65b884c0dfcdaf3cecf02eadd318c272
Mahaburalam <mahaburcse@gmail.com>
2023-12-04 15:24:13 +0600

    variable-datatype

---
+++ 2. JavaScript Fundamentals – Part 1/data-type.js
@@ -0,0 +1,36 @@
+/*
+type of data type:
+1. object
+2. primitive
+*/
+
+/* 
+primitive data type:
+1. Number: let age = 23
+2. String :let firstName = "Mahabur"
+3. Boolean : let fullAge = true;
+4. Undefine : let children;
+5. Null 
+6. Symbol
+7. BigInt
+*/
+
+// number data type
+let number = 1232;
+console.log("The given number is:",number);
+console.log("data type of this variable is: "+typeof number);
+
+// string
+let firstName = "Mahabur";
+console.log(firstName);
+console.log(typeof(firstName));
+
+// boolean
+let boolDataType = false;
+console.log(boolDataType);
+console.log(typeof boolDataType);
+
+// undefine
+let newDatatype;
+console.log(newDatatype);
+console.log(typeof(newDatatype));
---
+++ 2. JavaScript Fundamentals – Part 1/let-var-const.js
@@ -0,0 +1,20 @@
+// let - block scope
+// var - functional / global scope
+// conat - block scope
+
+var firstName = "Mahabur";
+firstName = "Tipu"
+var firstName = "Ahanaf"
+console.log(firstName);
+
+
+
+let secName = "Anas";
+// let secName = "Nuhash" - not allowed
+// secName = "Nuhash" - allowed
+console.log(secName);
+
+const joiningDate = 2021;
+// const  joiningDate = 4535; - not allowed for const variable
+// joiningDate = 4535; - not allowed
+console.log(joiningDate);
